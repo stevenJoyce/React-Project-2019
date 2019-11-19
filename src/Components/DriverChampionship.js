dd5fe867439b0a2ed375b8c1ba915d7b5e574602
Steven Joyce <g00362012@gmit.ie>
2019-11-19 17:40:49 +0000

    F1 Constructor and Driver record pages complete

--- src/Components/DriverChampionship.js
+++ src/Components/DriverChampionship.js
@@ -1,16 +1,306 @@
 import React from 'react';
-
+import ReactTable from "react-table";  
+import "react-table/react-table.css";  //A preset react css file
 class DriverChampionship extends React.Component {
   //To turn function to class add class name and extend app to include React.Component
-  //Put the return in a render
-  render(){
-    return (
-      <div className="App">
-        <h1> My Driver Championship in another component</h1>
-      </div>
-    );
-  }
-  
-}
-
+  //Put the return in a render  
+  render() {  
+     const data = [
+       {  
+        name: 'Michael Schumacher',
+        nationality: 'German', 
+        noOfChampionships: 7,
+        year: '1994, 1995, 2000, 2001, 2002, 2003, 2004',
+        wins: 91,
+        noOfSeasons: 19
+      },
+      {  
+        name: 'Lewis Hamilton',
+        nationality: 'English', 
+        noOfChampionships: 6,
+        year: '2008, 2014, 2015, 2017, 2018, 2019',
+        wins: 83,
+        noOfSeasons: 13
+      },
+      {  
+        name: 'Juan Manuel Fangio',  
+        nationality: 'Argentinian', 
+        noOfChampionships: 5,
+        year: '1051, 1954, 1955, 1956, 1957',
+        wins: 24,
+        noOfSeasons: 8 
+      },
+      {  
+        name: 'Alain Prost',  
+        nationality: 'French', 
+        noOfChampionships: 4,
+        year: '1985, 1986, 1989, 1993',
+        wins: 51,
+        noOfSeasons: 13      
+      },
+      {  
+        name: 'Sebastian Vettel',  
+        nationality: 'German', 
+        noOfChampionships: 4,
+        year: '2010, 2011, 2012, 2013',
+        wins: 53,
+        noOfSeasons: 13
+      },
+      {  
+        name: 'Ayrton Senna',  
+        nationality: 'Brazilian', 
+        noOfChampionships: 3,
+        year: '1988, 1989, 1991',
+        wins: 41,
+        noOfSeasons: 11 
+      },
+      {  
+        name: 'Jackie Stewart',  
+        nationality: 'English', 
+        noOfChampionships: 3,
+        year: '1969, 1971, 1973',
+        wins: 27,
+        noOfSeasons: 9
+      },
+      {  
+        name: 'Jack Brabham',  
+        nationality: 'Australian', 
+        noOfChampionships: 3,
+        year: '1959, 1960, 1966',
+        wins: 14,
+        noOfSeasons: 16 
+      },
+      {  
+        name: 'Brazil Nelson Piquet	',  
+        nationality: 'Brazilian', 
+        noOfChampionships: 3,
+        year: '1981, 1983, 1987',
+        wins: 23,
+        noOfSeasons: 14 
+      },
+      {  
+        name: 'Niki Lauda',  
+        nationality: 'Austrian', 
+        noOfChampionships: 3,
+        year: '1975, 1977, 1984',
+        wins: 25,
+        noOfSeasons: 13 
+      },
+      { 
+        name: 'Alberto Ascari	',  
+        nationality: 'Italian', 
+        noOfChampionships: 2,
+        year: '1952, 1953',
+        wins: 13,
+        noOfSeasons: 6 
+      },
+      {  
+        name: 'Graham Hill',  
+        nationality: 'English', 
+        noOfChampionships: 2,
+        year: '1962, 1968',
+        wins: 14,
+        noOfSeasons: 18 
+      },
+      {  
+        name: 'Emerson Fittipaldi',  
+        nationality: 'Brazilian', 
+        noOfChampionships: 2,
+        year: '1972, 1974',
+        wins: 14,
+        noOfSeasons: 11 
+      },
+      {  
+        name: 'Fernando Alonso',  
+        nationality: 'Spanish', 
+        noOfChampionships: 2,
+        year: '2005, 2006',
+        wins: 32,
+        noOfSeasons: 17 
+      },
+      {  
+        name: 'Mika Häkkinen',  
+        nationality: 'Finnish', 
+        noOfChampionships: 2,
+        year: '1998, 1999',
+        wins: 20,
+        noOfSeasons: 11 
+      },
+      {  
+        name: 'Jim Clark',  
+        nationality: 'Scottish', 
+        noOfChampionships: 2,
+        year: '1963, 1965',
+        wins: 25,
+        noOfSeasons: 9 
+      },
+      {  
+        name: 'Giuseppe Farina',  
+        nationality: 'Italian', 
+        noOfChampionships: 1,
+        year: '1950',
+        wins: 5,
+        noOfSeasons: 7 
+      },
+      {  
+        name: 'Mike Hawthorn',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '1958',
+        wins: 3,
+        noOfSeasons: 7 
+      },
+      {  
+        name: 'Jody Scheckter',  
+        nationality: 'South African', 
+        noOfChampionships: 1,
+        year: '1979',
+        wins: 10,
+        noOfSeasons: 9 
+      },
+      {  
+        name: 'Jacques Villeneuve',  
+        nationality: 'Canadian', 
+        noOfChampionships: 1,
+        year: '1997',
+        wins: 11,
+        noOfSeasons: 11 
+      },
+      {  
+        name: 'John Surtees',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '1964',
+        wins: 6,
+        noOfSeasons: 14 
+      },
+      {  
+        name: 'Phil Hill',  
+        nationality: 'American', 
+        noOfChampionships: 1,
+        year: '1961',
+        wins: 3,
+        noOfSeasons: 8 
+      },
+      {  
+        name: 'Keke Rosberg',  
+        nationality: 'Finnish', 
+        noOfChampionships: 1,
+        year: '1982',
+        wins: 5,
+        noOfSeasons: 9 
+      },
+      {  
+        name: 'Nigel Mansell',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '1992',
+        wins: 31,
+        noOfSeasons: 15 
+      },
+      {  
+        name: 'Jochen Rindt',  
+        nationality: 'Austrian', 
+        noOfChampionships: 1,
+        year: '1970',
+        wins: 6,
+        noOfSeasons: 7 
+      },
+      {  
+        name: 'Alan Jones',  
+        nationality: 'Australian', 
+        noOfChampionships: 1,
+        year: '1980',
+        wins: 12,
+        noOfSeasons: 10
+      },
+      {  
+        name: 'Damon Hill',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '1996',
+        wins: 22,
+        noOfSeasons: 8 
+      },
+      {  
+        name: 'Kimi Räikkönen',  
+        nationality: 'Finnish', 
+        noOfChampionships: 1,
+        year: '2007',
+        wins: 21,
+        noOfSeasons: 17 
+      },
+      {  
+        name: 'Denis Hulme',  
+        nationality: 'New Zealanders', 
+        noOfChampionships: 1,
+        year: '1967',
+        wins: 3,
+        noOfSeasons: 7 
+      },
+      {  
+        name: 'James Hunt',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '1976',
+        wins: 10,
+        noOfSeasons: 7 
+      },
+      {  
+        name: 'Mario Andretti',  
+        nationality: 'American', 
+        noOfChampionships: 1,
+        year: '1978',
+        wins: 12,
+        noOfSeasons: 15 
+      },
+      {  
+        name: 'Nico Rosberg',  
+        nationality: 'German', 
+        noOfChampionships: 1,
+        year: '2016',
+        wins: 23,
+        noOfSeasons: 11
+      },
+      {  
+        name: 'Jenson Button',  
+        nationality: 'English', 
+        noOfChampionships: 1,
+        year: '2009',
+        wins: 15,
+        noOfSeasons: 18
+      }
+    ]  
+     const columns = [
+       {  
+          Header: 'Name',  
+          accessor: 'name'  
+       },
+       {  
+         Header: 'Nationality',  
+         accessor: 'nationality'  
+       },
+       {  
+        Header: 'Year(s)',  
+        accessor: 'year'  
+      },
+      {  
+        Header: 'Number of Wins',  
+        accessor: 'wins'  
+      },
+      {  
+        Header: 'Number of Seasons',  
+        accessor: 'noOfSeasons'  
+      }
+      ]  
+    return (  
+          <div>  
+            <h1> All-Time F1 World Champions </h1>
+              <ReactTable  
+                  data={data}  
+                  columns={columns}   />  
+          </div>        
+    )  
+  }  
+}  
 export default DriverChampionship;
--- src/Components/TeamChampionship.js
+++ src/Components/TeamChampionship.js
@@ -1,16 +1,215 @@
 import React from 'react';
-
+import ReactTable from "react-table";  
+import "react-table/react-table.css"; //A preset react css file 
 class TeamChampionship extends React.Component {
   //To turn function to class add class name and extend app to include React.Component
   //Put the return in a render
-  render(){
-    return (
-      <div className="App">
-        <h1> My Team Championship in another component</h1>
-      </div>
-    );
-  }
-  
-}
+  render() {  
+    const data = [
+      {  
+       name: 'Ferrari',
+       firstSeason: 1950,
+       lastSeason: 2019, 
+       nationality: 'Italian', 
+       noOfChampionships: 16,
+       year: '1961, 1964, 1975, 1976, 1977, 1979, 1982, 1983, 1999, 2000, 2001, 2002, 2003, 2004, 2007, 2008',
+       wins: 238,
+       poles: 228,
+       noOfSeasons: 70
+     },
+     {  
+      name: 'Williams',
+      firstSeason: 1975,
+      lastSeason:2019, 
+      noOfChampionships: 9,
+      year: '1980, 1981, 1986, 1987, 1992, 1993, 1994, 1996, 1997',
+      wins: 114,
+      poles: 128,
+      noOfSeasons: 44
+     },
+     {  
+       name: 'McLaren',
+       firstSeason: 1966,
+       lastSeason:2019, 
+       noOfChampionships: 8,
+       year: '1974, 1984, 1985, 1988, 1989, 1990, 1991, 1998',
+       wins: 182,
+       poles: 155,
+       noOfSeasons: 54
+     },
+     {  
+      name: 'Lotus',
+      firstSeason: 1958,
+      lastSeason: 2015, 
+      noOfChampionships: 7,
+      year: '1963, 1965, 1968, 1970, 1972, 1973, 1978',
+      wins: 81,
+      poles: 76,
+      noOfSeasons: 43
+    },
+    {  
+      name: 'Mercedes',
+      firstSeason: 1954,
+      lastSeason: 2019,  
+      noOfChampionships: 6,
+      year: '2014, 2015, 2016, 2017, 2018, 2019',
+      wins: 101,
+      poles: 110,
+      noOfSeasons: 12
+    },
+    {  
+      name: 'Red Bull',
+      firstSeason: 2005,
+      lastSeason: 2019, 
+      noOfChampionships: 4,
+      year: '2010, 2011, 2012, 2013',
+      wins: 62,
+      poles: 62,
+      noOfSeasons: 15
+    },
+    {  
+      name: 'Cooper',
+      firstSeason: 1950,
+      lastSeason:1969, 
+      noOfChampionships: 2,
+      year: '1959, 1960',
+      wins: 16,
+      poles: 11,
+      noOfSeasons: 19
+    },
+    {  
+      name: 'Brabham',
+      firstSeason: 1962,
+      lastSeason: 1992, 
+      noOfChampionships: 2,
+      year: '1966, 1967',
+      wins: 35,
+      poles: 39,
+      noOfSeasons: 30
+    },
+    {  
+      name: 'Renault',
+      firstSeason: 1977,
+      lastSeason: 2019,  
+      noOfChampionships: 2,
+      year: '2005, 2006',
+      wins: 35,
+      poles: 51,
+      noOfSeasons: 23
+    },
+    {  
+      name: 'Benetton',
+      firstSeason: 1986,
+      lastSeason:2001, 
+      noOfChampionships: 1,
+      year: '1995',
+      wins: 27,
+      poles: 15,
+      noOfSeasons: 16
+    },
+    {  
+      name: 'Brawn GP',
+      firstSeason: 2009,
+      lastSeason: 2009, 
+      noOfChampionships: 1,
+      year: '2009',
+      wins: 8,
+      poles: 5,
+      noOfSeasons: 1
+    },
+    {  
+      name: 'BRM',
+      firstSeason: 1951,
+      lastSeason: 1977,  
+      noOfChampionships: 1,
+      year: '1962',
+      wins: 17,
+      poles: 11,
+      noOfSeasons: 23
+    },
+    {  
+      name: 'Matra',
+      firstSeason: 1967,
+      lastSeason:1972, 
+      noOfChampionships: 1,
+      year: '1969',
+      wins: 9,
+      poles: 4,
+      noOfSeasons: 6
+    },
+    {  
+      name: 'Tyrrell',
+      firstSeason: 1970,
+      lastSeason: 1998, 
+      noOfChampionships: 1,
+      year: '1971',
+      wins: 23,
+      poles: 14,
+      noOfSeasons: 29
+    },
+    {  
+      name: 'Vanwall',
+      firstSeason: 1954,
+      lastSeason:1960, 
+      noOfChampionships: 1,
+      year: '1958',
+      wins: 9,
+      poles: 7,
+      noOfSeasons: 7
+    },
+   ]  
+    const columns = [
+      {  
+         Header: 'Team Name',  
+         accessor: 'name' ,
+         width: 130 
+      },
+      {
+        Header: 'First Season',
+        accessor: 'firstSeason',
+        width: 100
+      },
+      {
+        Header: 'Last Season',
+        accessor: 'lastSeason',
+        width: 100
+      },
+      {  
+        Header: 'Titles',  
+        accessor: 'noOfChampionships', 
+        width: 100
+      },
+      {  
+       Header: 'Year(s)',  
+       accessor: 'year' ,
+       width:400,
+       style: { 'white-space': 'unset' }
+     },
+     {  
+       Header: 'Number of Wins',  
+       accessor: 'wins' ,
+       width: 150 
+     },
+     {
+        Header: 'Pole Position',
+        accessor: 'poles',
+        width: 100
+     },
+     {  
+       Header: 'Number of Seasons',  
+       accessor: 'noOfSeasons' , 
+       width:150
+     }
+     ]  
+   return (  
+         <div>  
+           <h1> All- Time F1 Constructor Champions </h1>
+             <ReactTable  
+                 data={data}  
+                 columns={columns}  />  
+         </div>        
+   )  
+ }  
+}  
 
 export default TeamChampionship;
